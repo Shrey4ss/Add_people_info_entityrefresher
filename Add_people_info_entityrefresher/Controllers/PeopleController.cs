fb4e6df09377a515050b1b3ff5202c365f7d7a74
Shreyas <sshivkar46@gmail.com>
2023-08-08 14:15:46 +0530

    Added fucntionality for the add people button in Home Page

---
+++ Add_people_info_entityrefresher/Controllers/PeopleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Add_people_info_entityrefresher.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
