215505a09e9981a49f3d6c243f55f7f42626c84a
Shreyas <sshivkar46@gmail.com>
2023-08-08 13:19:42 +0530

    Add project files.

---
+++ Add_people_info_entityrefresher/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Add_people_info_entityrefresher.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Add_people_info_entityrefresher.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Add_people_info_entityrefresher/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Add_people_info_entityrefresher.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
