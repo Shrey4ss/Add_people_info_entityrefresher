f0fedaf727d3bd5cfcaeafd8052235ceb4d7be3f
Shreyas <sshivkar46@gmail.com>
2023-08-08 14:07:11 +0530

    Added ApplicationDbContext and added first migration to create database

---
+++ Add_people_info_entityrefresher/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Add_people_info_entityrefresher.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Add_people_info_entityrefresher.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options) 
+        {
+            
+        }
+        public DbSet<People>Peoples { get; set; }
+    }
+}
---
+++ Add_people_info_entityrefresher/Migrations/20230808083514_Initial_migration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Add_people_info_entityrefresher.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Peoples",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Lname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    About = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Peoples", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Peoples");
+        }
+    }
+}
