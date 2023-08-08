6f14b3e04505d57b68120f1d5991d99a82172553
Shreyas <sshivkar46@gmail.com>
2023-08-08 13:42:11 +0530

    Added two buttons in home and People Model

---
+++ Add_people_info_entityrefresher/Models/People.cs
@@ -0,0 +1,13 @@
+﻿namespace Add_people_info_entityrefresher.Models
+{
+    public class People
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string Lname { get; set; }
+        public int Age { get; set; }
+        public string About { get; set; }
+
+
+    }
+}
